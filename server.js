a8aa77b63ce035d1fb4d6d0ef471b42c15504bd9
SHREYA-G-AMIN <shreyagamin@gmail.com>
2026-07-28 09:18:40 +0530

    Stage 0: create sqlite database

--- server.js
+++ server.js
@@ -4,6 +4,33 @@ const app = express();
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
+const sqlite3 = require("sqlite3").verbose();
+
+
+const db = new sqlite3.Database("./tasks.db", (err) => {
+  if (err) {
+    console.error("Database connection failed:", err.message);
+  } else {
+    console.log("Connected to SQLite database.");
+  }
+});
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS tasks (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      done INTEGER NOT NULL DEFAULT 0
+    )
+  `);
+  db.get("SELECT COUNT(*) AS count FROM tasks", (err, row) => {
+    if (row.count === 0) {
+      db.run("INSERT INTO tasks (title, done) VALUES (?, ?)", ["Learn Express", 0]);
+db.run("INSERT INTO tasks (title, done) VALUES (?, ?)", ["Build CRUD API", 0]);
+db.run("INSERT INTO tasks (title, done) VALUES (?, ?)", ["Push to GitHub", 1]);
+    }
+  });
+});
 
 app.use(express.json());
 
